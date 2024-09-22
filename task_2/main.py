03ebbebac2974bc23d8489f87efb68fcc728c1a2
Danemous1985 <s3578854@student.rmit.edu.au>
2024-09-23 01:39:41 +1000

    Added alot of stuff. Theres styling for index.html (first page). I like the UI design, its nice! I have made 1 query and it works good, no css on that results page yet though. Also all the uploading CSV stuff to gcloud BigQuery is done and working. Queries work good there too and reflect same results as my program! :). Doing good so far. Fixed some my commenting and reminder comments for where I'll put future stuff in the code! Also have a requirements.txt for installing needed stuff in virtual environment.

--- task_2/main.py
+++ task_2/main.py
@@ -3,25 +3,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+"""
+route returns main index.html page
+"""
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Query 1: Top 10 time slots with the highest trade value
+"""
+This is Query 1. It will show top 10 time slots with highest trade values like assignment brief ask's. I think the result looks correct?
+"""
 @app.route('/query1')
 def query1():
-    client = bigquery.Client()
+    client = bigquery.Client(project="task2-436212")
+    
+    # The query with field names, much like SQL query
     query = """
-    SELECT time_ref, SUM(import_value + export_value) AS trade_value
-    FROM `assignment1_dataset.gsquarterlySeptember20`
+    SELECT time_ref, SUM(value) AS trade_value
+    FROM `task2-436212.quarterly.quarterly_sep_20`
     GROUP BY time_ref
     ORDER BY trade_value DESC
     LIMIT 10
     """
+    
     query_job = client.query(query)
     results = query_job.result()
+    
     return render_template('results.html', results=results)
 
-# Add more query routes if needed
+
+# Leave comment here for add more queries from assignment
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
